dd0c3d0e7ebf4e1694b247e67cc19f463413f1fe
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-04-16 12:09:14 +0200

    exo 1

---
+++ public/js/main.js
@@ -0,0 +1,45 @@
+//array
+// let array = ["string", 43, true];
+// console.log(array);
+
+//number of elements, not caracter
+// console.log(array.length);
+
+//method
+// array.push("charles");
+// console.log(array);
+
+//unshift method -- ajout d'un element au début du array
+// array.unshift("schoum");
+// console.log(array);
+
+//slice method
+// array.splice(3, 0, "kevin"); //place avant et ne replace rien
+// console.log(array);
+
+// array.splice(4, 1, "charles"); //remplace celui désigner
+// console.log(array);
+
+// array.splice(4, 1, "charles", "loic"); //remplace celui désigner et en met deux à la place
+// console.log(array);
+
+
+// - # Exercice 1 JavaScript -> Array
+
+//     - ## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 11, coding 12, coding 13
+
+let arrayUn = ["coding 11", "coding 12", "coding 13"];
+
+//     - ##  Afficher les éléments de l'array
+
+console.log(arrayUn);
+
+//     - ## Afficher le nombre d'éléments de l'array
+
+console.log(arrayUn.length);
+
+//     - ## Afficher les éléments de l'array 1 par 1
+
+console.log(arrayUn[0]);
+console.log(arrayUn[1]);
+console.log(arrayUn[2]);
